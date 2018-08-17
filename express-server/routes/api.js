098823f63ec900ee977935f706cb543e8faa3892
Sothish Sasidharan <7797048+sothishs@users.noreply.github.com>
2018-08-17 15:43:28 -0500

    Add files via upload

---
+++ express-server/routes/api.js
@@ -0,0 +1,62 @@
+// Import dependencies
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+// MongoDB URL from the docker-compose file
+const dbHost = 'mongodb://database/mean-docker';
+
+// Connect to mongodb
+mongoose.connect(dbHost);
+
+// create mongoose schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  title: String
+});
+
+// create mongoose model
+const User = mongoose.model('User', userSchema);
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+		res.send('api works');
+});
+
+/* GET all users. */
+router.get('/users', (req, res) => {
+	User.find({}, (err, users) => {
+		if (err) res.status(500).send(error)
+
+		res.status(200).json(users);
+	});
+});
+
+/* GET one users. */
+router.get('/users/:id', (req, res) => {
+	User.findById(req.params.id, (err, users) => {
+		if (err) res.status(500).send(error)
+
+		res.status(200).json(users);
+	});
+});
+
+/* Create a user. */
+router.post('/users', (req, res) => {
+	let user = new User({
+		name: req.body.name,
+		price: req.body.price,
+		title: req.body.title
+	});
+
+	user.save(error => {
+		if (error) res.status(500).send(error);
+
+		res.status(201).json({
+			message: 'User created successfully'
+		});
+	});
+});
+
+module.exports = router;
